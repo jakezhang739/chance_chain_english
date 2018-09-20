93b65e8e4af679ba9d091713ac19a6208ac0cc3c
jakezhang739 <jakezhang739@gmail.com>
2018-09-20 18:19:07 -0400

    try face

--- app/src/main/java/com/example/jake/chance_chain/LoginActivity.java
+++ app/src/main/java/com/example/jake/chance_chain/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
@@ -17,25 +18,94 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity implements AWSLoginHandler {
 
+    private static final String TAG = "login";
     private Button loginBtn;
+    private LoginButton fbLog;
     private TextView signupText,forgotText;
     AWSLoginModel awsLoginModel;
     Boolean Show = false;
+    CallbackManager callbackManager;
+    PrefUtil prefUtil = new PrefUtil(LoginActivity.this);
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_login);
         ImageView toggleShow = (ImageView) findViewById(R.id.imageView7);
         loginBtn = (Button) findViewById(R.id.buttonLogIn);
+        fbLog = (LoginButton) findViewById(R.id.imageView6);
+        fbLog.setReadPermissions(Arrays.asList(
+                "public_profile", "email"));
+        fbLog.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+
+
+                        String accessToken = loginResult.getAccessToken().getToken();
+
+                        // save accessToken to SharedPreference
+                        prefUtil.saveAccessToken(accessToken);
+
+                        GraphRequest request = GraphRequest.newMeRequest(
+                                loginResult.getAccessToken(),
+                                new GraphRequest.GraphJSONObjectCallback() {
+                                    @Override
+                                    public void onCompleted(JSONObject jsonObject,
+                                                            GraphResponse response) {
+
+                                        // Getting FB User Data
+                                        Bundle facebookData = getFacebookData(jsonObject);
+
+
+                                    }
+                                });
+
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,first_name,last_name,email,gender");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+                    }
+
+
+                    @Override
+                    public void onCancel () {
+                        Log.d(TAG, "Login attempt cancelled.");
+                    }
+
+                    @Override
+                    public void onError (FacebookException e){
+                        e.printStackTrace();
+                        Log.d(TAG, "Login attempt failed.");
+                        deleteAccessToken();
+                    }
+                }
+        );
         signupText = (TextView) findViewById(R.id.textViewUserSignUp);
         forgotText = (TextView) findViewById(R.id.textViewUserForgotPassword);
         awsLoginModel = new AWSLoginModel(this, this);
@@ -78,6 +148,22 @@ public class LoginActivity extends AppCompatActivity implements AWSLoginHandler 
 
     }
 
+    private void deleteAccessToken() {
+        AccessTokenTracker accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(
+                    AccessToken oldAccessToken,
+                    AccessToken currentAccessToken) {
+
+                if (currentAccessToken == null){
+                    //User logged out
+                    prefUtil.clearToken();
+                    LoginManager.getInstance().logOut();
+                }
+            }
+        };
+    }
+
     private void LoginAction(){
         EditText login = (EditText) findViewById(R.id.editTextUserId);
         EditText password = (EditText) findViewById(R.id.editTextUserPassword);
@@ -107,12 +193,53 @@ public class LoginActivity extends AppCompatActivity implements AWSLoginHandler 
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
     public void onFailure(int process, Exception exception) {
 
         exception.printStackTrace();
 
         Toast.makeText(LoginActivity.this, R.string.login_failed, Toast.LENGTH_LONG).show();
 
+    }
+    private Bundle getFacebookData(JSONObject object) {
+        Bundle bundle = new Bundle();
+
+        try {
+            String id = object.getString("id");
+            URL profile_pic;
+            try {
+                profile_pic = new URL("https://graph.facebook.com/" + id + "/picture?type=large");
+                Log.i("profile_pic", profile_pic + "");
+                bundle.putString("profile_pic", profile_pic.toString());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            bundle.putString("idFacebook", id);
+            if (object.has("first_name"))
+                bundle.putString("first_name", object.getString("first_name"));
+            if (object.has("last_name"))
+                bundle.putString("last_name", object.getString("last_name"));
+            if (object.has("email"))
+                bundle.putString("email", object.getString("email"));
+            if (object.has("gender"))
+                bundle.putString("gender", object.getString("gender"));
+
+
+            prefUtil.saveFacebookUserInfo(object.getString("first_name"),
+                    object.getString("last_name"),object.getString("email"),
+                    object.getString("gender"), profile_pic.toString());
+
+        } catch (Exception e) {
+            Log.d(TAG, "BUNDLE Exception : "+e.toString());
+        }
+
+        return bundle;
     }
 
 }
